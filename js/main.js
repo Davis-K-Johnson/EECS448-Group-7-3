9d952d017c6652c2484770d1d6a3ad06bc4ce7e5
Michael Perry <michaelp018@gmail.com>
2021-10-27 20:29:30 -0500

    Dynamic Event Delegation for watch list elements

--- js/main.js
+++ js/main.js
@@ -15,8 +15,11 @@
 // DOMContentLoaded
 // Pull and initialize data for 
 document.addEventListener("DOMContentLoaded", () => { 
+    console.log("DOMContentLoaded test")
 
-})
+    // Event Delegation for Watch List UL element
+    document.getElementById("watch-list").addEventListener("click", (e) => handleWatchListClick(e));
+});
 
 // Event Listener Function for 1 Day Button
 
@@ -33,6 +36,19 @@ document.addEventListener("DOMContentLoaded", () => {
 // Event Listener Function for Search Bar Button
 
 // Event Listener Function for WatchList Currency Buttons
+function handleWatchListClick(e) {
+    // e.target is the clicked element!
+    // If it was a list item
+    if(e.target && e.target.nodeName == "A") {
+        // List item found!  Output the ID!
+        console.log("A type item was clicked!");
+        console.log(e.target);
+    }
+    else if (e.target && e.target.nodeName == "SPAN") {
+        console.log("span type item was clicked!");
+        console.log(e.target.parentElement);
+    }
+}
 
 // Event Listener Function for Add to WatchList Buttons
 
