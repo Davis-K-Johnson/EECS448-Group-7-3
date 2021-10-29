b9d11d4551bcbba4c327abf42167acd7dd1cf4a7
Michael Perry <michaelp018@gmail.com>
2021-10-29 14:31:29 -0500

    initial graph template

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
     //currencyHL.setQuoteData();
 
     // Update UI Data
-    //updateGraphHeader();
+    updateGraphElements();
 });
 
 // Event Listener Function for 1 Day Button
@@ -164,11 +164,58 @@ function updateGraphHeaderElements() {
 
 // Update Graph Function
 function updateGraph() {
-    
+
 }
 
 function updateGraphElements() {
+    var ctx = document.getElementById("graph-canvas").getContext("2d");
 
+    var myChart = new Chart(ctx, {
+    type: 'line',
+    options: {
+        scales: {
+        xAxes: [{
+            type: 'time',
+        }]
+        }
+    },
+    data: {
+        labels: ["2015-03-15T13:03:00Z", "2015-03-25T13:02:00Z", "2015-04-25T14:12:00Z"],
+        datasets: [{
+        label: 'Demo',
+        data: [{
+            t: '2015-03-15T13:03:00Z',
+            y: 12
+            },
+            {
+            t: '2015-03-25T13:02:00Z',
+            y: 21
+            },
+            {
+            t: '2015-04-25T14:12:00Z',
+            y: 32
+            }
+        ],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+        }]
+    }
+    });
 }
 
 
